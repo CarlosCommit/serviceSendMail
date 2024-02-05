9754a2afbe15034b9f2e5167564226d7c2840689
Carlos Mamani <carlos.mamani@aeolabs.io>
2024-02-04 22:22:22 -0300

    Se crea clases Dto para email y respuesta

---
+++ src/main/java/com/service/email/dto/ResponseDTO.java
@@ -0,0 +1,17 @@
+package com.service.email.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseDTO {
+
+    private String message;
+    private Integer status;
+    private Object payload;
+}
